6af7530459be6c31672bb0a4639ed0aff7a96146
pulse <antynomia@gmail.com>
2021-11-18 23:37:58 +0100

    initial clusters

--- gcp.tf
+++ gcp.tf
@@ -1,5 +1,19 @@
-provider "google" {
-  credentials = file("/config/api-exercise.json")
-  project     = var.project
-  region      = var.region
+ provider "google" {
+   credentials = file("/config/api-exercise.json")
+   project     = var.project
+   region      = var.region
+ }
+
+#provider "google" {
+#  credentials = file("/home/pulse/gke/api-exercise.json")
+#  project     = var.project
+#  region      = var.region
+#}
+
+terraform {
+  backend "gcs" {
+    credentials = "/config/api-exercise.json"
+    bucket = "qaztf-state-prod"
+    prefix = "qazterraform/state"
+  }
 }
